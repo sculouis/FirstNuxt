207ed9b4bf9b9254c7e7dfe8a68e444d3810d013
sculouis <scu.louis@gmail.com>
2021-05-28 20:28:40 +0800

    find products

--- pages/README.md
+++ pages/README.md
@@ -4,3 +4,16 @@ This directory contains your Application Views and Routes.
 The framework reads all the `*.vue` files inside this directory and creates the router of your application.
 
 More information about the usage of this directory in [the documentation](https://nuxtjs.org/guide/routing).
+
+## [vue 重要程式區塊](https://vuejs.org/v2/guide/index.html)
+
+1. props
+2. data
+3. methods
+4. computed
+
+## 陣列
+
+1. 讓 product component 依據取得的資料動態產生商品頁面
+2. 每個 row 固定 3 個 Coloum
+3. 自行決定每個 Row 有幾個 Column
--- pages/productlist.vue
+++ pages/productlist.vue
@@ -4,10 +4,18 @@
         護手霜商品總頁面
     </h6>
     <br>
-    <div class="row" v-for="products,index in productlist" v-bind:key="index">
+    <div class="row">
+        <div class="col">
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" v-model="searchText" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" v-on:click.prevent="searchProducts">Search</button>
+            </form>
+        </div>
+    </div>
+    <div class="row m-1" v-for="products,index in productlist2" v-bind:key="index">
         <div class="col" v-for="product,index in products" v-bind:key="index">
-            <product v-bind:text="product.text" v-bind:imgsrc="product.imgsrc">        
-            </product> 
+            <product v-bind:text="product.text" v-bind:imgsrc="product.imgsrc">
+            </product>
         </div>
     </div>
 </div>
@@ -15,51 +23,99 @@
 
 <script>
 export default {
-    data:function() {
-    return {
-        products:
-    [{
-        text:"護手霜A1款",
-        imgsrc:"https://cdn.bella.tw/files/AF24A07BEE-SP-7874082.jpg"
+    data: function () {
+        return {
+            products1: [{
+                    text: "護手霜A1款",
+                    imgsrc: "https://cdn.bella.tw/files/AF24A07BEE-SP-7874082.jpg"
+                },
+                {
+                    text: "護手霜A2款",
+                    imgsrc: "https://cdn.bella.tw/files/AF24A07BEE-SP-7874082.jpg"
+                },
+                {
+                    text: "護手霜A3款",
+                    imgsrc: "https://cdn.bella.tw/files/AF24A07BEE-SP-7874082.jpg"
+                },
+                {
+                    text: "護手霜B1款",
+                    imgsrc: "https://cdn.bella.tw/files/L%E2%80%99OCCITANE%E5%8E%9F%E9%87%8E%E4%B9%8B%E5%BF%83%E8%8A%B1%E8%88%9E%E8%AD%B7%E6%89%8B%E9%9C%9C%2030ml%EF%BC%8FNT_400.jpg"
+                },
+                {
+                    text: "護手霜B2款",
+                    imgsrc: "https://cdn.bella.tw/files/L%E2%80%99OCCITANE%E5%8E%9F%E9%87%8E%E4%B9%8B%E5%BF%83%E8%8A%B1%E8%88%9E%E8%AD%B7%E6%89%8B%E9%9C%9C%2030ml%EF%BC%8FNT_400.jpg"
+                },
+                {
+                    text: "護手霜B3款",
+                    imgsrc: "https://cdn.bella.tw/files/L%E2%80%99OCCITANE%E5%8E%9F%E9%87%8E%E4%B9%8B%E5%BF%83%E8%8A%B1%E8%88%9E%E8%AD%B7%E6%89%8B%E9%9C%9C%2030ml%EF%BC%8FNT_400.jpg"
+                }
+            ],
+            products2: [
+                [{
+                        text: "護手霜A1款",
+                        imgsrc: "https://cdn.bella.tw/files/AF24A07BEE-SP-7874082.jpg"
+                    },
+                    {
+                        text: "護手霜A2款",
+                        imgsrc: "https://cdn.bella.tw/files/AF24A07BEE-SP-7874082.jpg"
+                    },
+                    {
+                        text: "護手霜A3款",
+                        imgsrc: "https://cdn.bella.tw/files/AF24A07BEE-SP-7874082.jpg"
+                    }
+                ],
+                [{
+                        text: "護手霜B1款",
+                        imgsrc: "https://cdn.bella.tw/files/L%E2%80%99OCCITANE%E5%8E%9F%E9%87%8E%E4%B9%8B%E5%BF%83%E8%8A%B1%E8%88%9E%E8%AD%B7%E6%89%8B%E9%9C%9C%2030ml%EF%BC%8FNT_400.jpg"
+                    },
+                    {
+                        text: "護手霜B2款",
+                        imgsrc: "https://cdn.bella.tw/files/L%E2%80%99OCCITANE%E5%8E%9F%E9%87%8E%E4%B9%8B%E5%BF%83%E8%8A%B1%E8%88%9E%E8%AD%B7%E6%89%8B%E9%9C%9C%2030ml%EF%BC%8FNT_400.jpg"
+                    },
+                    {
+                        text: "護手霜B3款",
+                        imgsrc: "https://cdn.bella.tw/files/L%E2%80%99OCCITANE%E5%8E%9F%E9%87%8E%E4%B9%8B%E5%BF%83%E8%8A%B1%E8%88%9E%E8%AD%B7%E6%89%8B%E9%9C%9C%2030ml%EF%BC%8FNT_400.jpg"
+                    }
+                ]
+            ],
+            searchText: ""
+
+        };
+
     },
-    {
-        text:"護手霜A1款",
-        imgsrc:"https://cdn.bella.tw/files/AF24A07BEE-SP-7874082.jpg"
-    },
-        {
-        text:"護手霜A1款",
-        imgsrc:"https://cdn.bella.tw/files/AF24A07BEE-SP-7874082.jpg"
-    },
-        {
-        text:"護手霜B1款",
-        imgsrc:"https://cdn.bella.tw/files/L%E2%80%99OCCITANE%E5%8E%9F%E9%87%8E%E4%B9%8B%E5%BF%83%E8%8A%B1%E8%88%9E%E8%AD%B7%E6%89%8B%E9%9C%9C%2030ml%EF%BC%8FNT_400.jpg"
-    },
-            {
-        text:"護手霜B1款",
-        imgsrc:"https://cdn.bella.tw/files/L%E2%80%99OCCITANE%E5%8E%9F%E9%87%8E%E4%B9%8B%E5%BF%83%E8%8A%B1%E8%88%9E%E8%AD%B7%E6%89%8B%E9%9C%9C%2030ml%EF%BC%8FNT_400.jpg"
-    },
-        {
-        text:"護手霜B1款",
-        imgsrc:"https://cdn.bella.tw/files/L%E2%80%99OCCITANE%E5%8E%9F%E9%87%8E%E4%B9%8B%E5%BF%83%E8%8A%B1%E8%88%9E%E8%AD%B7%E6%89%8B%E9%9C%9C%2030ml%EF%BC%8FNT_400.jpg"
-    }
-    ]
-    }; 
-    
-    },
-    methods:{
+    methods: {
         //將陣列幾個分一組
-        sliceArray:function(arr, size) {
+        sliceArray: function (arr, size) {
             var arr2 = [];
-            for(var i=0; i<arr.length; i=i+size){
-                arr2.push(arr.slice(i,i+size));
+            for (var i = 0; i < arr.length; i = i + size) {
+                arr2.push(arr.slice(i, i + size));
             }
             return arr2;
+        },
+        //尋找相關產品
+        searchProducts: function () {
+            console.log(this.searchText);
+
         }
     },
-    computed:{
-        productlist:function(){
-            return this.sliceArray(this.products,2)
-        }
+    computed: {
+        productlist1: function () {
+            return this.sliceArray(this.products1, 3)
+        },
+        productlist2: function () {
+            var vm = this;
+            var arr = [];
+            if (vm.searchText != ""){
+                this.products1.forEach(function (item) {
+                    if (item.text.indexOf(vm.searchText) > 0) {
+                        arr.push(item);
+                    }
+                });
+            }else{
+                arr = this.products1;
+            }
+            return this.sliceArray(arr, 3)
+        },
     }
 }
 </script>
