9ee6cfa262e121ca959c14514878143afffa5331
sculouis <scu.louis@gmail.com>
2021-06-05 14:47:35 +0800

    add product

--- components/product.vue
+++ components/product.vue
@@ -1,27 +1,41 @@
 <template>
-  <div>
-  <b-card
-    v-bind:title="title"
-    v-bind:img-src="imgsrc"
-    img-alt="Image"
-    img-top
-    tag="article"
-    style="max-width: 20rem;"
-    class="mb-2"
-  >
-    <b-card-text>
-      {{text}}
-    </b-card-text>
+<div>
+    <b-card v-bind:title="title" v-bind:img-src="imgsrc" img-alt="Image" img-top tag="article" style="max-width: 20rem;" class="mb-2">
+        <b-card-text>
+            {{text}}
+        </b-card-text>
 
-    <b-button href="#" variant="primary">Go somewhere</b-button>
-  </b-card>
+        <b-button v-on:click="add()" variant="primary">＋</b-button>
+        <p>{{ count }}</p>
+        <b-button v-on:click="sub()" variant="primary">-</b-button>
+        <b-button variant="danger">加入購物車</b-button>
+    </b-card>
 </div>
 </template>
 
 <script>
 export default {
-    props:["text","imgsrc","title"]
+    props: ["text", "imgsrc", "title"],
+    data: function () {
+        return {
+            count: 1,
+            msg: ""
+        }
+    },
+    methods: {
+        add: function () {
+            this.count = this.count + 1;
+        },
+        sub: function () {
+            if (this.count > 1) {
+                this.count = this.count - 1;
+            } else {
+                this.count = 1
+            }
 
+        }
+
+    }
 }
 </script>
 
